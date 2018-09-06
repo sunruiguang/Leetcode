b5739af580352243f130d5a49d33b342f4bf906d
sunruiguang <srg.dandelion@gmail.com>
2018-09-06 20:58:27 +0800

    easy code

---
+++ src/com/srg/code/LongestValidParenthesesBruteForce.java
@@ -0,0 +1,36 @@
+package com.srg.code;
+
+import java.util.Stack;
+
+public class LongestValidParenthesesBruteForce {
+    public static void main(String[] args) {
+        String s = "()(()()";
+        System.out.println(longestValidParentheses(s));
+    }
+
+    private static int longestValidParentheses(String s) {
+        int maxlen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 2; j <= s.length(); j += 2) {
+                if (isValid(s.substring(i, j))) {
+                    maxlen = Math.max(maxlen, j - i);
+                }
+            }
+        }
+        return maxlen;
+    }
+
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else if (!stack.empty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}

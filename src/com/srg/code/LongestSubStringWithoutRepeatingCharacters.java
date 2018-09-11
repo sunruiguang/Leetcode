8e4e159ebbb7272d215f44b17295eb142d688113
sunruiguang <srg.dandelion@gmail.com>
2018-09-11 10:50:46 +0800

    Hard code

--- src/com/srg/code/LongestSubStringWithoutRepeatingCharacters.java
+++ src/com/srg/code/LongestSubStringWithoutRepeatingCharacters.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class LongestSubStringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
-        String s = "dvdf";
+        String s = "dvsdfppdf";
         System.out.println(lengthOfLongestSubstring(s));
     }
 
